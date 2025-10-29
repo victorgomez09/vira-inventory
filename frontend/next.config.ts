0e2f6ebace1ab409a9087c48bb48562676576787
Víctor Gómez <victorgomez1209@outlook.es>
2025-10-29 15:50:37 +0100

    change nextjs backend url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://backend:8000'
   },
   output: 'standalone'
 };
